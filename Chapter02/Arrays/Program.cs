58b30a1147818e59e2e7c1b0dc46db03d4c25cfe
Excavanor <str_token@example.com>
2021-09-15 13:07:26 +0300

    15.sept.2021

---
+++ Chapter02/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names; // может ссылаться на любой массив строк
+            // объявление размера массива
+            string[] names = new string[4];
+            // хранение элементов с индексами позиций
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+
+            for(int i = 0; i < names.Length; i++ ){
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88;
+            object name = "Barmaley";
+
+            Console.WriteLine($"{name} is {height} meters tall.");
+
+            // int length1 = name.Length;  ошибочная запись
+            int length2 = ((string)name).Length; 
+
+            Console.WriteLine($"{name} is {length2} characters.");
+
+            dynamic anotherName = "Barbambia";
+            int length1 = anotherName.Length; // ошибки нет
+
+            // локальные переменные
+            var population = 66_000_000;
+            var weight = 1.88;
+            var price = 2.55M;
+            var fruit = "Apples";
+            var letter = 'Z';
+            var happy = true;
+            
+            // значения по умолчанию
+            Console.WriteLine($"default(int) = {default(int)}");
+            Console.WriteLine($"default(bool) = {default(bool)}");
+            Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default(string) = {default(string)}");
+        }
+    }
+}
